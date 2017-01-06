8ba848c1f4e5480dc1bef7386db8db962f381df4
josh-acutest <josh.crossley@acutesales.co.uk>
2017-01-06 19:08:30 +0000

    jc

---
+++ MotorsportAdvertising/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MotorsportAdvertising.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MotorsportAdvertising/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MotorsportAdvertising.Startup))]
+namespace MotorsportAdvertising
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
